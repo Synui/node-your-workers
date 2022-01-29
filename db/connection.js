464bcd20fc3e694a098dd4418bc9ff4f38d5764c
Synui <jonathon.schoenholz@yahoo.com>
2022-01-29 13:59:41 -0500

    Tables created

--- db/connection.js
+++ db/connection.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'DB_USER',
-        password: 'DB-PW',
-        database: 'staff'
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME
     },
     console.log('Connected to the staff database')
 );
